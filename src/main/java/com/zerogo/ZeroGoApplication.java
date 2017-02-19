fa757665ad2fe50a4489ee72b71f9ca233a46e87
Administrator <13348867336@163.com>
2017-02-19 16:19:01 +0800

    First commit

---
+++ src/main/java/com/zerogo/ZeroGoApplication.java
@@ -0,0 +1,12 @@
+package com.zerogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroGoApplication.class, args);
+	}
+}
